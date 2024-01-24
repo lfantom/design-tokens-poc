532f249124a15bb9465aef78e2e0519df689861e
mjdenton <mjdenton@users.noreply.github.com>
2024-01-24 15:49:47 +0000

    Update Tokens

--- build/app/ios/semantic/_color.h
+++ build/app/ios/semantic/_color.h
@@ -4,18 +4,18 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 24 Jan 2024 15:42:51 GMT
+// Generated on Wed, 24 Jan 2024 15:49:46 GMT
 
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#define colorAccentDefault UIColor(red: 0.180, green: 0.545, blue: 0.341, alpha: 1)
-#define colorAccentMuted UIColor(red: 0.063, green: 0.094, blue: 0.157, alpha: 1)
 #define colorTextDefault UIColor(red: 0.278, green: 0.329, blue: 0.404, alpha: 1)
 #define colorTextHeading UIColor(red: 0.063, green: 0.094, blue: 0.157, alpha: 1)
 #define colorTextSubheading UIColor(red: 0.204, green: 0.251, blue: 0.329, alpha: 1)
 #define colorTextPlaceholder UIColor(red: 0.400, green: 0.439, blue: 0.522, alpha: 1)
 #define colorTextDisabled UIColor(red: 0.596, green: 0.635, blue: 0.702, alpha: 1)
 #define colorTextHover UIColor(red: 0.114, green: 0.161, blue: 0.224, alpha: 1)
+#define colorAccentDefault UIColor(red: 1.000, green: 0.498, blue: 0.314, alpha: 1)
+#define colorAccentMuted UIColor(red: 0.063, green: 0.094, blue: 0.157, alpha: 1)
 
